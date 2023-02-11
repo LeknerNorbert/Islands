31ef4fcd51bd00f6a761e100ce6433c48633cc4d
Norbert Lekner <leknern@kkszki.hu>
2023-02-11 18:06:27 +0100

    fix

--- Islands/Controllers/ClassifiedAdController.cs
+++ Islands/Controllers/ClassifiedAdController.cs
@@ -10,81 +10,81 @@ namespace Web.Controllers
     [ApiController]
     public class ClassifiedAdController : ControllerBase
     {
-        private readonly IClassifiedAdService _classifiedAdService;
+        //private readonly IClassifiedAdService _classifiedAdService;
 
-        public ClassifiedAdController(IClassifiedAdService classifiedAdService)
-        {
-            _classifiedAdService = classifiedAdService;
-        }
+        //public ClassifiedAdController(IClassifiedAdService classifiedAdService)
+        //{
+        //    _classifiedAdService = classifiedAdService;
+        //}
 
-        [HttpGet, Authorize(Roles = "User")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult GetClassifieds()
-        {
-            try
-            {
-                List<ClassifiedAdDto> classifieds = _classifiedAdService.GetClassifiedAds();
-                return Ok(classifieds);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
+        //[HttpGet, Authorize(Roles = "User")]
+        //[ProducesResponseType(StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
+        //public IActionResult GetClassifieds()
+        //{
+        //    try
+        //    {
+        //        List<ClassifiedAdDto> classifieds = _classifiedAdService.GetClassifiedAds();
+        //        return Ok(classifieds);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
 
-        [HttpGet, Authorize(Roles = "User")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult GetMyClassifieds()
-        {
-            try
-            {
-                string username = User.Claims.First(c => c.Type == "Username").Value;
-                List<ClassifiedAdDto> classifieds = _classifiedAdService.GetMyClassifiedAds(username);
+        //[HttpGet, Authorize(Roles = "User")]
+        //[ProducesResponseType(StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
+        //public IActionResult GetMyClassifieds()
+        //{
+        //    try
+        //    {
+        //        string username = User.Claims.First(c => c.Type == "Username").Value;
+        //        List<ClassifiedAdDto> classifieds = _classifiedAdService.GetMyClassifiedAds(username);
 
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
+        //        return Ok();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
 
-        [HttpPost, Authorize(Roles = "User")]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult CreateClassifiedAd(CreateClassifiedAdDto createClassifiedAd)
-        {
-            try
-            {
-                string username = User.Claims.First(c => c.Type == "Username").Value;
-                _classifiedAdService.CreateClassifiedAd(username, createClassifiedAd);
+        //[HttpPost, Authorize(Roles = "User")]
+        //[ProducesResponseType(StatusCodes.Status201Created)]
+        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
+        //public IActionResult CreateClassifiedAd(CreateClassifiedAdDto createClassifiedAd)
+        //{
+        //    try
+        //    {
+        //        string username = User.Claims.First(c => c.Type == "Username").Value;
+        //        _classifiedAdService.CreateClassifiedAd(username, createClassifiedAd);
 
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
+        //        return Ok();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
 
-        [HttpDelete, Authorize(Roles = "User")]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult DeleteClassifiedAd(int id)
-        {
-            try
-            {
-                string username = User.Claims.First(c => c.Type == "Username").Value;
-                _classifiedAdService.DeleteClassifiedAd(id);
+        //[HttpDelete, Authorize(Roles = "User")]
+        //[ProducesResponseType(StatusCodes.Status201Created)]
+        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
+        //public IActionResult DeleteClassifiedAd(int id)
+        //{
+        //    try
+        //    {
+        //        string username = User.Claims.First(c => c.Type == "Username").Value;
+        //        _classifiedAdService.DeleteClassifiedAd(id);
 
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
+        //        return Ok();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
     }
 }
