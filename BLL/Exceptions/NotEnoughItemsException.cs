db42b1871b5066c3efdc504e8d5ee0de89cdda74
Norbert Lekner <leknern@kkszki.hu>
2023-03-12 03:42:43 +0100

    Hello World

--- BLL/Exceptions/NotEnoughItemsException.cs
+++ BLL/Exceptions/NotEnoughItemsException.cs
@@ -11,5 +11,6 @@ namespace BLL.Exceptions
         public NotEnoughItemsException() { }
         public NotEnoughItemsException(string message) : base(message) { }
         public NotEnoughItemsException(string message, Exception inner) : base(message, inner) { }
+
     }
 }
