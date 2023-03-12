bc9abd75da699870ba9070e4ad30d88ecd8c2e49
Norbert Lekner <leknern@kkszki.hu>
2023-03-12 03:48:05 +0100

    FINALLY

--- BLL/Exceptions/EntityNotFoundException.cs
+++ BLL/Exceptions/EntityNotFoundException.cs
@@ -3,6 +3,7 @@
     public class EntityNotFoundException : Exception
     {
         public EntityNotFoundException() { }
+
         public EntityNotFoundException(string message) : base(message) { }
         public EntityNotFoundException(string message, Exception inner) : base(message, inner) { }
     }
