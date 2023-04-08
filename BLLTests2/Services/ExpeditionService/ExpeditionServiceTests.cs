cf6b36ac207384f1437528da5154a796443e258a
szigilie@kkszki.hu <szigilie@kkszki.hu>
2023-04-09 01:39:04 +0200

    BattleServiceTests OK!

--- BLLTests2/Services/ExpeditionService/ExpeditionServiceTests.cs
+++ BLLTests2/Services/ExpeditionService/ExpeditionServiceTests.cs
@@ -5,22 +5,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BLL.Services.RandomProvider;
+using Moq;
 
 namespace BLL.Services.ExpeditionService.Tests
 {
     [TestClass()]
     public class ExpeditionServiceTests
     {
+        // Random mock
+        private readonly Mock<IRandomProvider> mockRandomProvider = new();
 
         [TestMethod()]
         public void LootCalcTest()
         {
+            //Arrange
+
+            // Az általad megadott számot fogja visszaadni mindig randomként
+            // Random mock
+            mockRandomProvider.Setup(randomProvider =>
+                randomProvider.GetRandomNumber(It.IsAny<int>(), It.IsAny<int>())).Returns(10);
+
+            //Act
+
+            //Assert
             Assert.Fail();
         }
 
         [TestMethod()]
         public void CoinCalcTest()
         {
+            //Arrange
+
+            // Az általad megadott számot fogja visszaadni mindig randomként
+            // Random mock
+            mockRandomProvider.Setup(randomProvider =>
+                randomProvider.GetRandomNumber(It.IsAny<int>(), It.IsAny<int>())).Returns(10);
+
+            //Act
+
+            //Assert
             Assert.Fail();
         }
     }
